be34a02bdc2b17121869c9582901bce82846cada
Patryk Kosieradzki <patryk.kosieradzki@gmail.com>
2022-07-05 22:32:49 +0200

    Adds new workflows and configuration

---
+++ buildSrc/src/main/java/com/patrykkosieradzki/composer/buildsrc/Configuration.kt
@@ -0,0 +1,14 @@
+package com.patrykkosieradzki.composer.buildsrc
+
+object Configuration {
+    private const val majorVersion = 1
+    private const val minorVersion = 0
+    private const val patchVersion = 0
+
+    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+    const val versionCode = majorVersion * 1000 + minorVersion * 100 + patchVersion
+    const val snapshotVersionName = "$versionName-SNAPSHOT"
+
+    const val artifactGroup = "com.patrykkosieradzki"
+    const val artifactId = "composer"
+}
