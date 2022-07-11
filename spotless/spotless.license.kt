d28e4f0a3c1ab9a794450efb55e5947c5c0eba20
Patryk Kosieradzki <patryk.kosieradzki@gmail.com>
2022-07-11 22:46:45 +0200

    Fixes linter and adds spotless

--- spotless/spotless.license.kt
+++ spotless/spotless.license.kt
@@ -13,6 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.patrykkosieradzki.composer.core.event
-
-interface UiEvent
